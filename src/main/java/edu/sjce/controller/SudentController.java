00e7cd94d3eee70ded5c5339fc2f951ff67e404c
Vishnu Vardan E <vishnuvardan279@gmail.com>
2025-03-09 19:47:29 +0530

    Update Working Rest

--- src/main/java/edu/sjce/controller/SudentController.java
+++ src/main/java/edu/sjce/controller/SudentController.java
@@ -2,6 +2,7 @@ package edu.sjce.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 public class SudentController {
 	
-	@GetMapping("/student/getProfile")
-	public  StudentDetails getStudentDetails(@RequestParam String studentId){
+	@GetMapping("/student/{studentId}/getprofile")
+	public  StudentDetails getStudentDetails(@PathVariable String studentId){
 		StudentDetails studentDetails = new StudentDetails();
 		studentDetails.setBranch("CSC");
 		studentDetails.setDegree("BE");
@@ -27,29 +28,29 @@ public class SudentController {
 		return studentDetails;
 	}
 	
-	@PutMapping("/student/createProfile")
+	@PutMapping("/student/createprofile")
 	public boolean createStudentDetails(@RequestBody StudentDetails studentDetails) {
 		System.out.println("Student Id : " + studentDetails.getStudentId());
-		System.out.println("Student name : " +studentDetails.getName());
-		System.out.println("Student year : " +studentDetails.getYear());
+		System.out.println("Student name : " + studentDetails.getName());
+		System.out.println("Student year : " + studentDetails.getYear());
 		System.out.println("student branch : " + studentDetails.getBranch());
 		System.out.println("Student Degree : " + studentDetails.getDegree());
 		return true;
 	}
 	
-	@PostMapping("/student/updateprofile")
-	public boolean updateStudentDetails(@RequestBody StudentDetails studentDetails) {
+	@PostMapping("/student/{studentId}/updateprofile")
+	public boolean updateStudentDetails(@PathVariable String studentId, @RequestBody StudentDetails studentDetails) {
 		
-		System.out.println("Student Id : " + studentDetails.getStudentId());
-		System.out.println("Student name : " +studentDetails.getName());
-		System.out.println("Student year : " +studentDetails.getYear());
+		System.out.println("Student Id : " + studentId);
+		System.out.println("Student name : " + studentDetails.getName());
+		System.out.println("Student year : " + studentDetails.getYear());
 		System.out.println("student branch : " + studentDetails.getBranch());
 		System.out.println("Student Degree : " + studentDetails.getDegree());
 		return true;
 	}
 	
-	@DeleteMapping("/student/deleteprofile")
-	public boolean deletestudentDetails(@RequestParam String studentId) {
+	@DeleteMapping("/student/{studentId}/deleteprofile")
+	public boolean deletestudentDetails(@PathVariable String studentId) {
 		System.out.println("The Given StudentID was Deleted" + studentId);
 		return true;
 	}
